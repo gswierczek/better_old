20a1b9a7862f07194886e2f2716668ba21df7c4a
Grzegorz Swierczek <g.swierczek@gmail.com>
2017-11-16 15:09:27 +0100

    Initial commit

---
+++ src/main/java/com/codistica/better/config/BetterApplication.java
@@ -0,0 +1,12 @@
+package com.codistica.better.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterApplication.class, args);
+	}
+}
